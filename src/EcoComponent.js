a95b029d96a9eb3d64024f27f8b9918ba8346849
David López <dalo_vidpez@hotmail.com.ar>
2025-09-22 19:56:59 -0300

    Corregida identación en EcoComponent

--- src/EcoComponent.js
+++ src/EcoComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 function EcoComponent(){
-    return <div>
+    return (
+    <div>
         <h2>Ante la falta de reservas, el Central decide imprimir dólares</h2>
         <p>"―Todos sabemos que para terminar con la pobreza solamente hay que fabricar más dinero― aseguró el presidente del BCRA</p>
-    </div>;
+    </div>
+    );
 }
 
 export default EcoComponent;
