8a38197a2c395e9abdd0a20bc9e932247d2152c1
David López <dalo_vidpez@hotmail.com.ar>
2025-09-22 19:41:26 -0300

    Añadida noticia económica

---
+++ src/EcoComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EcoComponent(){
+    return 
+    <div>
+        <h2>Ante la falta de reservas, el Central decide imprimir dólares</h2>
+        <p>"―Todos sabemos que para terminar con la pobreza solamente hay que fabricar más dinero― aseguró el presidente del BCRA</p>
+    </div>;
+}
+
+export default EcoComponent;
